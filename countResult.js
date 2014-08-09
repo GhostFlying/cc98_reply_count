83b5fe9abdded8cabdf42eef9d0696c30ce57d66
GhostFlying <GhostFlying@Gmail.com>
2014-08-09 16:58:58 +0800

    Change print order and add file store.

--- countResult.js
+++ countResult.js
@@ -1,6 +1,8 @@
 /**
  * Created by ghostflying on 14-8-7.
  */
+var fs = require("fs");
+
 var userCount = function (username) {
     this.name = username;
     this.count = 0;
@@ -26,9 +28,40 @@ countStorage.prototype.addCount = function (username) {
 };
 
 countStorage.prototype.print = function () {
-    for (var each in this.storage) {
-        console.log (each + " : " + this.storage[each].count);
+    var array = this.toArray();
+    var writeStr = "";
+    var filename = new Date().toLocaleTimeString().replace(/:/g, "_") + ".txt";
+
+    array.sort(this.compareFunction);
+    for (var i = 0; i < array.length; i ++) {
+        writeStr += array[i].name + " : " + array[i].count + "\r\n";
     }
+    console.log ("start write to file.");
+    console.log (writeStr);
+    fs.writeFile(filename, writeStr, function (err) {
+        if (err)
+            throw  err;
+        console.log ("result file saved. " + filename);
+    });
+}
+
+countStorage.prototype.toArray = function () {
+    var array = [];
+
+    for (var each in this.storage) {
+        array.push(this.storage[each]);
+    }
+    return array;
+}
+
+countStorage.prototype.compareFunction = function (a, b) {
+    if (a.count > b.count){
+        return - 1;
+    }
+    if (a.count < b.count){
+        return 1;
+    }
+    return 0;
 }
 
 module.exports = countStorage;
